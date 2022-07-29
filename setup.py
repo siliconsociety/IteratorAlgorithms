1b8f40480daf0e558d752c65625df2390c6328e2
BrokenShell <webmaster@sharpdesigndigital.com>
2022-07-28 21:42:57 -0700

    v0.1.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     py_modules=["IteratorAlgorithms"],
     author="Robert Sharp",
     author_email="webmaster@sharpdesigndigital.com",
-    version="0.1.5",
+    version="0.1.6",
     description="A collection of iterator algorithms for Python3.",
     long_description=long_description,
     long_description_content_type="text/markdown",
